e66760622308b2195a6e118c2ebf88e765838f05
Andrei Melnik <andrei.melnik@qdeck.com>
2025-06-19 15:13:59 -0700

    updated logging levels

--- logger/net_logger.py
+++ logger/net_logger.py
@@ -9,7 +9,7 @@ py_logger.setLevel(logging.DEBUG)
 
 class PythonLogger:
     def Log(self, level, msg):
-        py_logger.log(logging.INFO, msg)
+        py_logger.log(level * 10, msg)
 
 
 net_logger = PythonLoggerAdapter(PythonLogger())
